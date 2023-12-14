83ede266a9c999fc092aa82d8da3937f28b9ce83
Zertimond <91977198+Zertimond@users.noreply.github.com>
2023-12-15 00:52:30 +0200

    Added additional files

---
+++ Lab1_Web/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Common.Models;
+using Lab1_Data.Entities;
+
+namespace Lab1_Web.Services;
+
+public interface IUserService
+{
+    Task<Guid> CreateUser(UserCreateModel model);
+    Task DeleteUser(UserDeleteModel model);
+    Task UpdateUser(UserUpdateModel model);
+    Task<List<User>> GetAllUsers(int page, int pageSize, string? sortBy, bool? isAsc);
+}
